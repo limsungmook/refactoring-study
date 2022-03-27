0243d86d820de3971e2dec035ce273b136fc8eaa
limsungmook <ipes4579@gmail.com>
2022-03-28 01:28:39 +0900

    4.4 테스트 추가하기 - 중복 코드를 바깥 범위로 끌어냄
    - 이렇게 하면 안되지만 일단 넣음

--- chapter04-test/test/province.test.mjs
+++ chapter04-test/test/province.test.mjs
@@ -3,13 +3,12 @@ import { sampleProvinceData } from "../sampleProvinceData.mjs";
 import { expect } from "chai";
 
 describe("province", function () {
+  const asia = new Province(sampleProvinceData());
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
 
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
